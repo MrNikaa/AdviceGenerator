47f16d5d2af104a3199287224f6efdb80b58f574
Nika Panjavidze <panjavidzenika@gmail.com>
2023-06-15 15:01:13 +0400

    Added protection
    added try catch

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const diceImage = document.querySelector("#dice-image");
 
 rollButton.addEventListener("click", async () => 
 {
+    try
+    {
     const response = await axios.get(`https://api.adviceslip.com/advice`);
     let adviceText = response.data.slip.advice;
     let adviceNum = response.data.slip.id;
     diceImage.classList.add('dice-animation');
     adviceNumber.innerText = `ADVICE #${adviceNum}`;
     adviceDisplay.innerText = adviceText;
+    }catch(e)
+    {
+        adviceDisplay.innerText =  "Error! API Is not working."
+        adviceNumber.innerText = "ADVICE #ERROR"
+    }
 });
 
 // Remove the animation class after the animation ends
